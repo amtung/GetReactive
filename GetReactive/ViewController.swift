4b6965d8ac3a5074adeef9dd46bd997650aaf5b0
Annie Tung <annie.mmt@gmail.com>
2018-02-13 00:45:03 -0500

    simple tableview, search bar, array that reacts to data changes

--- GetReactive/ViewController.swift
+++ GetReactive/ViewController.swift
@@ -7,19 +7,72 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+
+    let disposeBag = DisposeBag()
+    var citiesToShow = [String]()
+    let arrayOfCities = ["New York", "Hong Kong", "London", "Tokyo", "Seoul"]
+    let arrayOfNames = Variable(["Annie"])
+
+    // MARK: - ViewController lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        searchBar
+            .rx.text
+            .orEmpty
+            .debounce(0.5, scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .subscribe(onNext: { [unowned self] query in // we want to be notified for every new value
+                self.citiesToShow = self.arrayOfCities.filter { $0.hasPrefix(query) }
+                self.tableView.reloadData()
+            }).disposed(by: disposeBag)
+
+        arrayOfNames.asObservable()
+            .filter { value in
+                return value.count == 1
+            }
+            //.debug()
+            .map { value in
+                return "user is: \(value)"
+            }
+            .subscribe(onNext: { value in
+                print(value)
+            }).disposed(by: disposeBag)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        arrayOfNames.value = ["Annie", "Ben"]
+        arrayOfNames.value = ["Cassie", "Dan"]
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        arrayOfNames.value = ["Emily"]
+    }
 
+    // MARK: - TableView
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return citiesToShow.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CityCell", for: indexPath)
+        cell.textLabel?.text = citiesToShow[indexPath.row]
+        return cell
+    }
 }
 
